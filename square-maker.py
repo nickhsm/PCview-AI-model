248a4c802027ae841c5514385db0efde94071354
nickhsm <r0938000@student.thomasmore.be>
2025-04-27 13:54:14 +0200

    Remove comments

--- square-maker.py
+++ square-maker.py
@@ -4,21 +4,6 @@ import glob
 import os
 
 
-# Search images in directory
-# Load 1 image
-# Edit
-# Make new one
-# Delete old one
-# Move new to old position
-# Repeat
-
-files = glob.glob("./data/*/*")
-
-for file in files:
-    if file.count("/") > 3:
-        print(file)
-        raise ValueError("Dit kan niet. Er is meer nested folders dan verwacht.")
-
 def square_maker(file):
     """
     This will add padding to an image to make it square.
@@ -35,29 +20,15 @@ def square_maker(file):
         os.remove(file)
         os.rename(new_filename, file)
 
+files = glob.glob("./data/*/*")
+
+for file in files:
+    if file.count("/") > 3:
+        print(file)
+        raise ValueError("Dit kan niet. Er is meer nested folders dan verwacht.")
+
 counter = 0
 for file in files:
     square_maker(file)
     counter += 1
     print(f"I have edited {counter} files", end="\r")
-
-
-#def loadImages(path):
-#    # return array of images
-#
-#    imagesList = listdir(path)
-#    loadedImages = []
-#    for image in imagesList:
-#        img = Image.open(path + image)
-#        loadedImages.append(img)
-#
-#    return loadedImages
-#
-#path = "data/"
-#
-## your images in an array
-#imgs = loadImages(path)
-#
-#for img in imgs:
-#    # you can show every image
-#    img.show()
