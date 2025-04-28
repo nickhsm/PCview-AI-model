62f4b5194b7f1e834d6da3531cf3412b216db4ec
nickhsm <r0938000@student.thomasmore.be>
2025-04-28 11:20:31 +0200

    Added more convolution for more details and bigger final neural network (dense)

--- classification.py
+++ classification.py
@@ -72,6 +72,10 @@ num_classes = len(class_names)
 model = Sequential([
     data_augmentation,
     layers.Rescaling(1./255),
+    layers.Conv2D(4, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(8, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
     layers.Conv2D(16, 3, padding='same', activation='relu'),
     layers.MaxPooling2D(),
     layers.Conv2D(32, 3, padding='same', activation='relu'),
@@ -80,9 +84,10 @@ model = Sequential([
     layers.MaxPooling2D(),
     layers.Dropout(0.2),
     layers.Flatten(),
-    layers.Dense(128, activation='relu'),
+    layers.Dense(256, activation='relu'),
     layers.Dense(num_classes, name="outputs")
     ])
+
 
 # Compile the model
 model.compile(optimizer='adam',
